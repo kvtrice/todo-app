51ccac4cb741fae997e4ddc50c54755215110519
kvtrice <katricemountford@gmail.com>
2024-09-04 15:37:46 +1000

    Runs clean build

--- backend/todo/src/main/java/io/nology/todo/TodoApplication.java
+++ backend/todo/src/main/java/io/nology/todo/TodoApplication.java
@@ -9,10 +9,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class TodoApplication {
 
 	public static void main(String[] args) {
-		// Dotenv dotenv = Dotenv.configure().load();
-		// System.setProperty("spring.datasource.url", dotenv.get("DB_DATASOURCE"));
-		// System.setProperty("spring.datasource.username", dotenv.get("DB_USERNAME"));
-		// System.setProperty("spring.datasource.password", dotenv.get("DB_PASSWORD"));
 
 		SpringApplication.run(TodoApplication.class, args);
 	}
