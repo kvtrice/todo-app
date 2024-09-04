4d302df8d67d5f5846f675fe40da5f79aae0f482
kvtrice <katricemountford@gmail.com>
2024-09-04 16:45:02 +1000

    Updated allowed domain to resolve CORS issues

--- backend/todo/src/main/java/io/nology/todo/config/WebConfig.java
+++ backend/todo/src/main/java/io/nology/todo/config/WebConfig.java
@@ -7,7 +7,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
     public void addCorsMappings(@SuppressWarnings("null") CorsRegistry registry) {
-        String[] allowedOrigins = { "http://localhost:5173/", "http://127.0.0.1:5173/" };
+        String[] allowedOrigins = { "https://kats-todo-app.vercel.app/" };
         registry.addMapping("/**").allowedOrigins(allowedOrigins).allowedMethods("*").allowedHeaders("*");
     }
 }
