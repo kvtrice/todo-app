270fb2d56e23ea2c9ce66dfae2eb1f742c8fed78
kvtrice <katricemountford@gmail.com>
2024-09-04 16:57:24 +1000

    Updated allowedOrigins

--- backend/todo/src/main/java/io/nology/todo/config/WebConfig.java
+++ backend/todo/src/main/java/io/nology/todo/config/WebConfig.java
@@ -7,7 +7,8 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
     public void addCorsMappings(@SuppressWarnings("null") CorsRegistry registry) {
-        String[] allowedOrigins = { "https://kats-todo-app.vercel.app/" };
+        String[] allowedOrigins = { "https://kats-todo-app.vercel.app/", "http://localhost:5173/",
+                "http://127.0.0.1:5173/" };
         registry.addMapping("/**").allowedOrigins(allowedOrigins).allowedMethods("*").allowedHeaders("*");
     }
 }
--- src/components/ToDoMain/ToDoMain.tsx
+++ src/components/ToDoMain/ToDoMain.tsx
@@ -1,8 +1,11 @@
+import useCardContext from "../../hooks/useCardContext";
 import FilterBar from "../FilterBar/FilterBar";
 import StatusLists from "../StatusLists/StatusLists";
 import styles from "./ToDoMain.module.scss";
 
 const ToDoMain = () => {
+	const { cards } = useCardContext();
+
 	return (
 		<div className={styles.main}>
 			<FilterBar />
